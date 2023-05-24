aca5717ce6d6c6abbe9144d4bde72a75645ab2e5
Maxwellagu <maxwellebuka.agu@gmail.com>
2023-05-24 15:12:46 +0100

    Removed id

--- SeoPieShop/Models/Category.cs
+++ SeoPieShop/Models/Category.cs
@@ -4,6 +4,7 @@ namespace SeoPieShop.Models
 {
     public class Category
     {
+        //public int Id { get; set; }
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
         public string Description { get; set; }
